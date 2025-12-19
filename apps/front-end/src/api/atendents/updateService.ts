d1d7e925111884f3460c3e451386ebd56c05cd31
unknown <caiomagalhaesdefaria@hotmail.com>
2025-12-19 15:22:34 -0300

    fix

---
+++ apps/front-end/src/api/atendents/updateService.ts
@@ -0,0 +1,22 @@
+import { AxiosError } from "axios"
+import { Api } from "../axios";
+
+export type UpdateServicePayload = {
+  description: string;
+  price: number;
+}
+
+export async function updateAtendentService(
+  atendentServiceId: string, 
+  payload: UpdateServicePayload
+): Promise<void> {
+  try {
+    await Api().put(`/atendent-service/${atendentServiceId}`, payload)
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error('Erro ao atualizar serviço:', error.message)
+    }
+    throw error
+  }
+}
+
